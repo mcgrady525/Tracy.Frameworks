a372df3c4235d0da34d809a51800b8a229e42869
mcgrady_525 <mcgrady_525@163.com>
2016-07-19 16:39:00 +0800

    分类，单向散列加密。

--- DEV/Tracy.Frameworks/Tracy.Frameworks.Common.Tests/SecurityExtensionTest.cs
+++ DEV/Tracy.Frameworks/Tracy.Frameworks.Common.Tests/SecurityExtensionTest.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Security.Cryptography;
+using System.Text;
+using Tracy.Frameworks.Common.Extends;
 
 namespace Tracy.Frameworks.Common.Tests
 {
@@ -18,5 +21,35 @@ namespace Tracy.Frameworks.Common.Tests
             System.Diagnostics.Debug.WriteLine("end");
 
         }
+
+        [NUnit.Framework.Test]
+        public void TestMD5()
+        {
+            var input = "admin";
+
+            var result1 = input.To16bitMD5();
+            var result2 = input.To32bitMD5();
+            var result4 = GetMD5String(input);
+
+        }
+
+        /// <summary>
+        /// MD5加密字符串
+        /// </summary>
+        public static string GetMD5String(string str)
+        {
+            MD5 md5 = new MD5CryptoServiceProvider();
+            byte[] data = System.Text.Encoding.Default.GetBytes(str);
+            byte[] md5data = md5.ComputeHash(data);
+            md5.Clear();
+
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < md5data.Length - 1; i++)
+            {
+                builder.Append(md5data[i].ToString("X2"));
+            }
+            return builder.ToString();
+        }
+
     }
 }
