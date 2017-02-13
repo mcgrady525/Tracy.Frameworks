2d4c4eb0c805f841aca5f9a1483262779d2cac4e
mcgrady_525 <mcgrady_525@163.com>
2017-02-13 10:20:03 +0800

    添加Tracy.Frameworks.LogClient.Tests项目。

---
+++ DEV/Tracy.Frameworks/Tracy.Frameworks.LogClient.Tests/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using NUnit.Framework;
+using Tracy.Frameworks.LogClient.Entity;
+using Tracy.Frameworks.Common.Extends;
+
+namespace Tracy.Frameworks.LogClient.Tests
+{
+    [NUnit.Framework.TestFixture]
+    public class UnitTest1
+    {
+        [NUnit.Framework.Test]
+        public void TestMethod1()
+        {
+            var debugLog = new DebugLog 
+            {
+                SystemCode= "Log",
+                Source= "Log.Service",
+                Message= "Message1",
+                Detail = "Detail1"
+            };
+            var result = debugLog.ToJson();
+        }
+    }
+}
