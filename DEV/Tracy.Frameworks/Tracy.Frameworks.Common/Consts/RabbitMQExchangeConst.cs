02ccb9f21e58a4ab113e11809e0e73ffe3b4f44b
mcgrady_525 <mcgrady_525@163.com>
2017-02-14 14:05:58 +0800

    添加rabbitMQ交换机和队列命名常量。

---
+++ DEV/Tracy.Frameworks/Tracy.Frameworks.Common/Consts/RabbitMQExchangeConst.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tracy.Frameworks.Common.Consts
+{
+    /// <summary>
+    /// 各业务系统的rabbitMQ交换机命名
+    /// </summary>
+    [Serializable]
+    public static class RabbitMQExchangeConst
+    {
+        /// <summary>
+        /// Log系统的debug log的rabbitMQ交换机名称
+        /// </summary>
+        public const string LogDebugLog = "Log.Exchange.DebugLog";
+
+        /// <summary>
+        /// Log系统的error log的rabbitMQ交换机名称
+        /// </summary>
+        public const string LogErrorLog = "Log.Exchange.ErrorLog";
+
+        /// <summary>
+        /// Log系统的xml log的rabbitMQ交换机名称
+        /// </summary>
+        public const string LogXmlLog = "Log.Exchange.XmlLog";
+
+        /// <summary>
+        /// Log系统的performance log的rabbitMQ交换机名称
+        /// </summary>
+        public const string LogPerformanceLog = "Log.Exchange.PerformanceLog";
+
+        /// <summary>
+        /// Log系统的trace log的rabbitMQ交换机名称
+        /// </summary>
+        public const string LogTraceLog = "Log.Exchange.TraceLog";
+
+    }
+}
---
+++ DEV/Tracy.Frameworks/Tracy.Frameworks.Common/Consts/RabbitMQQueueConst.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tracy.Frameworks.Common.Consts
+{
+    /// <summary>
+    /// 各业务系统的rabbitMQ队列命名
+    /// </summary>
+    [Serializable]
+    public static class RabbitMQQueueConst
+    {
+        /// <summary>
+        /// Log系统的debug log的rabbitMQ队列名称
+        /// </summary>
+        public const string LogDebugLog = "Log.Queue.DebugLog";
+
+        /// <summary>
+        /// Log系统的error log的rabbitMQ队列名称
+        /// </summary>
+        public const string LogErrorLog = "Log.Queue.ErrorLog";
+
+        /// <summary>
+        /// Log系统的xml log的rabbitMQ队列名称
+        /// </summary>
+        public const string LogXmlLog = "Log.Queue.XmlLog";
+
+        /// <summary>
+        /// Log系统的performance log的rabbitMQ队列名称
+        /// </summary>
+        public const string LogPerformanceLog = "Log.Queue.PerformanceLog";
+
+        /// <summary>
+        /// Log系统的trace log的rabbitMQ队列名称
+        /// </summary>
+        public const string LogTraceLog = "Log.Queue.TraceLog";
+
+    }
+}
