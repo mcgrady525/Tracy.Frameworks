cd572748131980c9b98e97fdf2b539bd6a656574
mcgrady_525 <mcgrady_525@163.com>
2017-02-14 10:04:01 +0800

    ...

--- DEV/Tracy.Frameworks/Tracy.Frameworks.Console.Tests/Program.cs
+++ DEV/Tracy.Frameworks/Tracy.Frameworks.Console.Tests/Program.cs
@@ -9,6 +9,50 @@ namespace Tracy.Frameworks.Console.Tests
     {
         static void Main(string[] args)
         {
+            System.Console.WriteLine("111");
+
+            //消费debug log
+            System.Threading.Tasks.Task.Factory.StartNew(() =>
+            {
+                ConsumerDebugLogMessage();
+            });
+
+            //消费error log
+            System.Threading.Tasks.Task.Factory.StartNew(() =>
+            {
+                ConsumerErrorLogMessage();
+            });
+
+            System.Console.WriteLine("是否为后台线程："+ System.Threading.Thread.CurrentThread.IsBackground.ToString());
+            System.Console.WriteLine("222");
+            System.Console.ReadKey();
         }
+
+        private static void ConsumerDebugLogMessage()
+        {
+            System.Console.WriteLine("aaa");
+
+            while (true)
+            {
+                System.Threading.Thread.Sleep(1000);
+                System.Console.WriteLine("sss");
+            }
+            
+            System.Console.WriteLine("bbb");
+        }
+
+        private static void ConsumerErrorLogMessage()
+        {
+            System.Console.WriteLine("ccc");
+
+            while (true)
+            {
+                System.Threading.Thread.Sleep(1000);
+                System.Console.WriteLine("ttt");
+            }
+
+            System.Console.WriteLine("ddd");
+        }
+
     }
 }
--- DEV/Tracy.Frameworks/Tracy.Frameworks.LogClient.Tests/UnitTest1.cs
+++ DEV/Tracy.Frameworks/Tracy.Frameworks.LogClient.Tests/UnitTest1.cs
@@ -2,6 +2,7 @@
 using NUnit.Framework;
 using Tracy.Frameworks.LogClient.Entity;
 using Tracy.Frameworks.Common.Extends;
+using System.Collections.Generic;
 
 namespace Tracy.Frameworks.LogClient.Tests
 {
@@ -11,14 +12,27 @@ namespace Tracy.Frameworks.LogClient.Tests
         [NUnit.Framework.Test]
         public void TestMethod1()
         {
-            var debugLog = new DebugLog 
+            //var debugLog = new DebugLog 
+            //{
+            //    SystemCode= "Log",
+            //    Source= "Log.Service",
+            //    Message= "Message1",
+            //    Detail = "Detail1"
+            //};
+            //var result = debugLog.ToJson();
+            var list = new List<DebugLog>();
+            for (int i = 0; i < 10; i++)
             {
-                SystemCode= "Log",
-                Source= "Log.Service",
-                Message= "Message1",
-                Detail = "Detail1"
-            };
-            var result = debugLog.ToJson();
+                list.Add(new DebugLog 
+                {
+                   SystemCode= "Log", 
+                   Source= "Log.Service",
+                   Message = "Message"+ i,
+                   Detail = "Detail"+ i
+                });
+            }
+            var result = list.ToJson();
         }
+
     }
 }
