af04bc1e4c20575a63a1d239963cc4d3a5a49f95
mcgrady_525 <mcgrady_525@163.com>
2017-03-08 10:04:37 +0800

    ...

--- DEV/Tracy.Frameworks/Tracy.Frameworks.LogClient.Tests/UnitTest1.cs
+++ DEV/Tracy.Frameworks/Tracy.Frameworks.LogClient.Tests/UnitTest1.cs
@@ -12,25 +12,17 @@ namespace Tracy.Frameworks.LogClient.Tests
         [NUnit.Framework.Test]
         public void TestMethod1()
         {
-            //var debugLog = new DebugLog 
-            //{
-            //    SystemCode= "Log",
-            //    Source= "Log.Service",
-            //    Message= "Message1",
-            //    Detail = "Detail1"
-            //};
-            //var result = debugLog.ToJson();
-            //var list = new List<DebugLog>();
-            //for (int i = 0; i < 10; i++)
-            //{
-            //    list.Add(new DebugLog 
-            //    {
-            //       SystemCode= "Log", 
-            //       Source= "Log.Service",
-            //       Message = "Message"+ i,
-            //       Detail = "Detail"+ i
-            //    });
-            //}
+            var list = new List<DebugLog>();
+            for (int i = 0; i < 10; i++)
+            {
+                list.Add(new DebugLog
+                {
+                    SystemCode = "Interface",
+                    Source = "Interface.Service",
+                    Message = "Message" + i,
+                    Detail = "Detail" + i
+                });
+            }
             //var list = new List<ErrorLog>();
             //for (int i = 0; i < 10; i++)
             //{
@@ -42,20 +34,20 @@ namespace Tracy.Frameworks.LogClient.Tests
             //        Detail = "Detail"+i
             //    });
             //}
-            var list = new List<XmlLog>();
-            for (int i = 0; i < 10; i++)
-            {
-                list.Add(new XmlLog
-                {
-                    SystemCode = "Log",
-                    Source = "Log.Task",
-                    ClassName = "ClassName" + i,
-                    MethodName = "MethodName" + i,
-                    RQ = "RQ" + i,
-                    RS = "RS" + i,
-                    Remark = "Remark" + i
-                });
-            }
+            //var list = new List<XmlLog>();
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    list.Add(new XmlLog
+            //    {
+            //        SystemCode = "Ubtrip",
+            //        Source = "Ubtrip.UI",
+            //        ClassName = "ClassName" + i,
+            //        MethodName = "MethodName" + i,
+            //        RQ = "RQ" + i,
+            //        RS = "RS" + i,
+            //        Remark = "Remark" + i
+            //    });
+            //}
             var result = list.ToJson();
         }
 
