429b2477dc814bd5f6456dcad787692e86aab27e
mcgrady_525 <mcgrady_525@163.com>
2017-03-09 12:28:47 +0800

    add SystemCode常量。

---
+++ DEV/Tracy.Frameworks/Tracy.Frameworks.LogClient/SystemCodeConst.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tracy.Frameworks.LogClient
+{
+    /// <summary>
+    /// SystemCode常量,系统代码
+    /// </summary>
+    public static class SystemCodeConst
+    {
+        public const string UBTRIP = "Ubtrip";//商旅系统
+
+        public const string NET = "Net";//网管系统
+
+        public const string INTERFACE = "Interface";//接口平台
+
+    }
+}
