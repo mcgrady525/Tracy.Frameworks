16568f8cdeab0428d81ef3d85fd2685e59ebb141
mcgrady_525 <mcgrady_525@163.com>
2017-03-29 19:10:13 +0800

    add RabbitMQ组件封装。

---
+++ DEV/Tracy.Frameworks/Tracy.Frameworks.RabbitMQ/RabbitMQConfig.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Tracy.Frameworks.RabbitMQ
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class RabbitMQConfig
+    {
+        public string Host { get; set; }
+        public ushort HeartBeat { get; set; }
+        public bool AutomaticRecoveryEnabled { get; set; }
+        public TimeSpan NetworkRecoveryInterval { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+
+    /// <summary>
+    /// 死信队列实体
+    /// </summary>
+    [RabbitMQQueue("dead-letter-{Queue}", ExchangeName = "dead-letter-{exchange}")]
+    public class DeadLetterQueue
+    {
+        public string Body { get; set; }
+
+        public string Exchange { get; set; }
+
+        public string Queue { get; set; }
+
+        public string RoutingKey { get; set; }
+
+        public int RetryCount { get; set; }
+
+        public string ExceptionMsg { get; set; }
+
+        public DateTime CreateDateTime { get; set; }
+    }
+}
---
+++ DEV/Tracy.Frameworks/Tracy.Frameworks.RabbitMQ/RabbitMqQueueAttribute.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Tracy.Frameworks.RabbitMQ
+{
+    /// <summary>
+    /// 自定义的RabbitMq队列信息实体特性
+    /// </summary>
+    public class RabbitMQQueueAttribute : Attribute
+    {
+        public RabbitMQQueueAttribute(string queueName)
+        {
+            QueueName = queueName ?? string.Empty;
+        }
+
+        /// <summary>
+        /// 交换机名称
+        /// </summary>
+        public string ExchangeName { get; set; }
+
+        /// <summary>
+        /// 队列名称
+        /// </summary>
+        public string QueueName { get; private set; }
+
+        /// <summary>
+        /// 是否持久化
+        /// </summary>
+        public bool IsProperties { get; set; }
+    }
+}
