4795d5cb245d84f90a3aeb5c69c1c4f647e9eef3
mcgrady525 <mcgrady_525@163.com>
2017-04-29 22:20:01 +0800

    ...

---
+++ DEV/Tracy.Frameworks/Tracy.Frameworks.Console.Tests/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Tracy.Frameworks.Console.Tests
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void Test1()
+        {
+            var s = "hello world!";
+
+        }
+
+    }
+}
