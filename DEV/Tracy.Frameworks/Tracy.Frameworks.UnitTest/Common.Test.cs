ad1776e379243d23aeac5c7f094df1137174371b
mcgrady525 <mcgrady_525@163.com>
2017-04-29 22:28:30 +0800

    add单元测试项目Tracy.Frameworks.UnitTest。

---
+++ DEV/Tracy.Frameworks/Tracy.Frameworks.UnitTest/Common.Test.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Tracy.Frameworks.Common.Extends;
+
+namespace Tracy.Frameworks.UnitTest
+{
+    /// <summary>
+    /// Tracy.Frameworks.Common的单元测试类
+    /// </summary>
+    [TestFixture]
+    public class CommonTest
+    {
+
+
+    }
+}
