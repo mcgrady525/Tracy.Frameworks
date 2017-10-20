fc2b5d8282567d7ef72ae57f1314443bd9b212f0
luning <luning@ssharing.com>
2017-10-20 22:41:17 +0800

    add redis的单元测试。

---
+++ DEV/Tracy.Frameworks/Tracy.Frameworks.UnitTest/Redis.Test.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tracy.Frameworks.Redis;
+using Tracy.Frameworks.Common.Extends;
+
+namespace Tracy.Frameworks.UnitTest
+{
+    /// <summary>
+    /// redis测试
+    /// </summary>
+    [TestFixture]
+    public class Redis
+    {
+        [Test]
+        public void Test_String()
+        {
+            var redisClient = new RedisWrapper();
+
+            //get
+            var name = redisClient.Get("name");
+
+            if (name.IsNullOrEmpty())
+            {
+                redisClient.GetOrAdd("name",()=> { return "kobe"; });
+            }
+
+            //add
+            redisClient.Add("","");
+
+        }
+
+
+    }
+}
